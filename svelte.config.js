fad4f97795b105a8de962603a6ee5ff3a84958d9
oavelino <oavelino@mit.edu>
2024-03-06 09:51:19 -0500

    updated base

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? 'my-portfolio' : process.env.BASE_PATH
 		}
 	}
 };
