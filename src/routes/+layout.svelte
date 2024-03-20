c3f18aef79963ed99aa93edb3f7206da238e95c8
oavelino <oavelino@mit.edu>
2024-03-19 22:32:26 -0400

    fixed nav bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+YOLO <slot />
+
+<script>
+    let pages = [
+        {url: "./", title: "Home"},
+        {url: "./projects", title: "Projects"},
+        {url: "./resume", title: "Resume"},
+        {url: "./contact", title: "Contact"},
+        {url: "https://github.mit.edu/oavelino", title: "Github"}
+    ]
+</script>
+<nav>
+	{#each pages as p }
+		<a href="p.url">p.title</a>
+	{/each}
+</nav>
