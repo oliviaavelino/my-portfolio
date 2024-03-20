e21b4a9b99903e1391f93d4299f3241bb9cf6f49
oavelino <oavelino@mit.edu>
2024-03-19 22:55:37 -0400

    one more time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
 <script>
     let pages = [
         {url: "./", title: "Home"},
-        {url: "./projects", title: "Projects"},
-        {url: "./resume", title: "Resume"},
-        {url: "./contact", title: "Contact"},
+        {url: "projects", title: "Projects"},
+        {url: "resume", title: "Resume"},
+        {url: "contact", title: "Contact"},
         {url: "https://github.mit.edu/oavelino", title: "Github"}
     ]
 </script>
