ca6f88ba05f438c2b14fb41001dccd364bebc4dc
oavelino <oavelino@mit.edu>
2024-04-03 12:45:47 -0400

    added meta file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
         {url: "projects", title: "Projects"},
         {url: "resume", title: "Resume"},
         {url: "contact", title: "Contact"},
+        {url: "meta", title: "Meta"},
         {url: "https://github.mit.edu/oavelino", title: "Github"}
     ]
 
